5d97cf64711c0b72dfa298cefad20cb111b4901c
shanky <shanky@shanky.local>
2012-02-05 15:51:15 -0500

    readme

--- config.py
+++ config.py
@@ -5,16 +5,20 @@ pyVersion 2.7
 Google_storage='gs'
 Buckets = []
 
-##In case there are multiple runs then
-##aggregation follows below priority for status
-##ie., if 1st run is completed but second is Failed,overall status is failed
+"""
+In case there are multiple runs then
+aggregation follows below priority for status
+ie., if 1st run is completed but second is Failed,overall status is failed
+"""
 Status_priority = ['Failed', 'In Progress', 'Completed']
+
+## regular expression patterns ##
 Status_log_pattern='Status-%s-(.*)-p(.*).log'
-
-Migration_categories = ['Calendar', 'Contact', 'Email']
-
 Attachment_large_pattern='Mail too large(.*)80041066(.*)'
 Disallowed_file_type_pattern='GDSTATUS_BAD_REQUEST:Permanent failure(.*)'
+
 ## error pattern -> category mapping
 Error_categories = {Attachment_large_pattern : 'Attachments too large',
                     Disallowed_file_type_pattern: 'Disallowed file types'}
+
+Migration_categories = ['Calendar', 'Contact', 'Email']
---
+++ readme.txt
@@ -0,0 +1,22 @@
+setup
+======
+
+1. please run ./configure to setup the configuration for project
+   this is a simple script prompting for your google storage access key
+
+limitations
+===========
+1. As per the google limitation on cloud storage, files greater than 32 MB
+   are not currently handled. There is a workaround by using files and reading
+   less than 32 MB but is currently not implemented by this project.
+
+
+ThirdParty
+==========
+1. Boto (2.2.1)  :- https://github.com/boto/boto
+2. oauth:- ( package included in gsutil )
+
+https://developers.google.com/storage/docs/gsutil
+
+Note:- Don't use boto provided by gsutil, as it has a conflict with HTTPSConnection 
+       class provided by google appengine. This is resolved in the above release
